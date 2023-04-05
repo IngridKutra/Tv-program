2c5cc0208348a8ca824bd5c92ef5be8798186ba2
Ingrid <ingrid.lia.testing@gmail.com>
2023-04-05 17:26:40 +0200

    trying to fix JSON file

--- script.js
+++ script.js
@@ -38,6 +38,7 @@
             document.getElementById("js-schedule").innerHTML = '';
             var imgLoading = document.getElementById("js-loading");
             imgLoading.setAttribute("class","loading-spinner");    
+            
             function filtrering(data){
                 console.log(data);
                 let tid = data.filter((passandeTid) => new Date(passandeTid.start) > new Date());
